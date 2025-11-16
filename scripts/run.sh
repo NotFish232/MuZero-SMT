70ed2fdaebdb0dff2fdeac702519c074e9c9b8c7
Justin Lee <justin.adam.lee@gmail.com>
2025-11-16 11:34:53 -0500

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
         
-# SBATCH --nodes=1    
-# SBATCH --ntasks=1                     
-# SBARCH --cpus-per-task=32
-# SBATCH --mem=64000
-# SBATCH --gpus=h100:1
-# SBATCH --time=1-00:00:00     
+#SBATCH --nodes=1    
+#SBATCH --ntasks=1                     
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64000
+#SBATCH --gpus=h100:1
+#SBATCH --time=1-00:00:00     
 
 
 source ~/venv/bin/activate
