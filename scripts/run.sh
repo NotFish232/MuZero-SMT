ff9e7ac582b6bfe28639ee935730959bd8ed84b6
Justin Lee <justin.adam.lee@gmail.com>
2025-11-16 11:31:51 -0500

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,13 @@
-srun --nodes=1 --cpus-per-task=8 --gpus=h100:1 --mem=32000 python3 train.py
+#!/usr/bin/env bash
+        
+# SBATCH --nodes=1    
+# SBATCH --ntasks=1                     
+# SBARCH --cpus-per-task=32
+# SBATCH --mem=64000
+# SBATCH --gpus=h100:1
+# SBATCH --time=1-00:00:00     
+
+
+source ~/venv/bin/activate
+
+python train.py
