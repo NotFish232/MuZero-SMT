2e0b1761a3d9917f757cd5b3119c46d318d8304f
Justin Lee <justin.adam.lee@gmail.com>
2025-11-17 23:55:08 -0500

    update test sbatch

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-
-#SBATCH --job-name=muzero_smt_eval
+        
+#SBATCH --job-name=smt_test
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 
+#SBATCH --exclusive
 #SBATCH --nodes=1    
 #SBATCH --ntasks=1                     
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64000
-#SBATCH --gpus=h100:1
-#SBATCH --time=1-00:00:00     
-
+#SBATCH --cpus-per-task=192
+#sbatch --mem=0
+#SBATCH --time=1-00:00:00    
 
 source venv/bin/activate
 
