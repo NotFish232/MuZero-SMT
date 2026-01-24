5fb448aae3e9c1590a3d6d0c92a9241c7e8a4649
Justin Lee <justin.adam.lee@gmail.com>
2026-01-24 17:58:05 -0500

    make big model

--- mu_zero_smt/models/graph_network.py
+++ mu_zero_smt/models/graph_network.py
@@ -16,38 +16,54 @@ class GraphEncoder(nn.Module):
         in_channels: int,
         hidden_channels: int,
         out_channels: int,
-    ) -> None:
+        num_layers: int,
+    ):
         super().__init__()
 
-        self.conv1 = TGnn.GINConv(
-            nn.Sequential(
-                nn.Linear(in_channels, hidden_channels),
-                nn.ReLU(),
-                nn.Linear(hidden_channels, hidden_channels),
-            ),
-            train_eps=True,
+        self.input_proj = nn.Linear(in_channels, hidden_channels)
+
+        self.convs = nn.ModuleList(
+            TGnn.GINConv(
+                nn.Sequential(
+                    nn.Linear(hidden_channels, hidden_channels),
+                    nn.ReLU(),
+                    nn.Linear(hidden_channels, hidden_channels),
+                ),
+                train_eps=True,
+            )
+            for _ in range(num_layers)
+        )
+        self.norms = nn.ModuleList(
+            nn.LayerNorm(hidden_channels) for _ in range(num_layers)
         )
 
-        self.conv2 = TGnn.GINConv(
-            nn.Sequential(
+        self.pool = TGnn.aggr.AttentionalAggregation(
+            gate_nn=nn.Sequential(
                 nn.Linear(hidden_channels, hidden_channels),
                 nn.ReLU(),
-                nn.Linear(hidden_channels, hidden_channels),
-            ),
-            train_eps=True,
+                nn.Linear(hidden_channels, 1),
+            )
         )
 
-        self.lin = nn.Linear(hidden_channels, out_channels)
+        self.output_proj = nn.Sequential(
+            nn.Linear(hidden_channels, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, out_channels),
+        )
 
-    def forward(self: Self, x, edge_index: T.Tensor, batch: T.Tensor) -> T.Tensor:
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
+    def forward(
+        self: Self, x: T.Tensor, edge_index: T.Tensor, batch: T.Tensor
+    ) -> T.Tensor:
+        x = self.input_proj(x)
 
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
+        for conv, norm in zip(self.convs, self.norms):
+            h = conv(x, edge_index)
+            h = norm(h)
+            x = x + F.relu(h)
 
-        x = TGnn.global_mean_pool(x, batch)
-        x = self.lin(x)
+        x = self.pool(x, batch)
+
+        x = self.output_proj(x)
 
         return x
 
@@ -67,16 +83,21 @@ class GraphNetwork(MuZeroNetwork):
         observation_size: int,
         action_space: list[int],
         encoded_state_size: int,
-        fc_reward_layers: list[int],
-        fc_value_layers: list[int],
-        fc_policy_layers: list[int],
-        fc_dynamics_layers: list[int],
         support_size: int,
+        representation_hidden_channels: int,
+        representation_num_layers: int,
+        dynamics_layers: list[int],
+        dynamics_heads_layers: list[int],
+        policy_layers: list[int],
+        policy_discrete_head_layers: list[int],
+        policy_continuous_heads_layers: list[int],
+        value_layers: list[int],
+        reward_layers: list[int],
     ) -> None:
         super().__init__()
 
+        self.observation_size = observation_size
         self.action_space = action_space
-
         self.encoded_state_size = encoded_state_size
 
         # Size of entire support with a support for values in range -[support_size, support_size]
@@ -84,18 +105,25 @@ class GraphNetwork(MuZeroNetwork):
 
         # Representation model
         self.representation_model = GraphEncoder(
-            observation_size, observation_size // 4, self.encoded_state_size
+            self.observation_size,
+            representation_hidden_channels,
+            self.encoded_state_size,
+            num_layers=representation_num_layers,
         )
 
         # Dynamics Model
         self.dynamics_model = mlp(
             self.encoded_state_size,
-            fc_dynamics_layers,
+            dynamics_layers,
             self.encoded_state_size,
         )
         self.dynamics_heads = nn.ModuleList(
             [
-                mlp(self.encoded_state_size + dim, [], self.encoded_state_size)
+                mlp(
+                    self.encoded_state_size + dim,
+                    dynamics_heads_layers,
+                    self.encoded_state_size,
+                )
                 for dim in self.action_space
             ]
         )
@@ -103,26 +131,29 @@ class GraphNetwork(MuZeroNetwork):
         # Policy model
         self.policy_model = mlp(
             self.encoded_state_size,
-            fc_policy_layers,
+            policy_layers,
             self.encoded_state_size,
         )
         self.policy_discrete_head = mlp(
             self.encoded_state_size,
-            fc_policy_layers,
+            policy_discrete_head_layers,
             len(self.action_space),
         )
         self.policy_continuous_heads = nn.ModuleList(
-            [mlp(self.encoded_state_size, [], 2 * dim) for dim in self.action_space]
+            [
+                mlp(self.encoded_state_size, policy_continuous_heads_layers, 2 * dim)
+                for dim in self.action_space
+            ]
         )
 
         # Value model
         self.value_model = mlp(
-            self.encoded_state_size, fc_value_layers, self.full_support_size
+            self.encoded_state_size, value_layers, self.full_support_size
         )
 
         # Reward model
         self.reward_model = mlp(
-            self.encoded_state_size, fc_reward_layers, self.full_support_size
+            self.encoded_state_size, reward_layers, self.full_support_size
         )
 
     def prediction(
@@ -236,7 +267,8 @@ class GraphNetwork(MuZeroNetwork):
 
                 batch = T.concat((batch_states, batch_params), dim=-1)
 
-                next_encoded_state[action_mask] = head(batch)
+                # Residual connection
+                next_encoded_state[action_mask] = head(batch) + batch_states
 
             cur_param_idx += dim
 
