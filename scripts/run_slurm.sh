62896bace77fe3a14f4eda97b78a9fe8ee5d81e4
Justin Lee <justin.adam.lee@gmail.com>
2026-01-31 18:32:36 -0500

    single script

---
+++ scripts/run_slurm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+file="$1"
+shift
+
+job_name="$(basename "$file" .py)"
+
+sbatch \
+    --job-name="$job_name" \
+    --output="%x_%j.out" \
+    --error="%x_%j.err" \
+    --exclusive \
+    --nodes=1 \
+    --ntasks=1 \
+    --cpus-per-task=192 \
+    --time=1-00:00:00 \
+    --wrap="source venv/bin/activate && python -u \"$file\" $*"
