4467b81290a8f182f724cc911a24409c20d6ff49
Justin Lee <justin.adam.lee@gmail.com>
2026-04-06 15:40:09 -0400

    back to 1 day

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -5,13 +5,6 @@ shift
 
 job_name="$(basename "$file" .py)"
 
-# Allocate more time for train
-if [ "$job_name" = "train" ]; then
-    time_req="3-00:00:00"
-else
-    time_req="1-00:00:00"
-fi
-
 sbatch \
     --job-name="MuZeroSMT_$job_name" \
     --output="%x_%j.out" \
@@ -20,5 +13,5 @@ sbatch \
     --ntasks=1 \
     --cpus-per-task=16 \
     --mem=128G \
-    --time="$time_req" \
+    --time="1-00:00:00" \
     --wrap="./venv/bin/python -u \"$file\" $*"
